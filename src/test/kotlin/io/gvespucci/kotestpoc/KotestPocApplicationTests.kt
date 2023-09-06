d2bedbb46fba2fac82358de09d4cf87c87b7a2d2
gvespucci <giorgio.vespucci@gmail.com>
2023-09-06 20:02:42 +0200

    chore: first commit

---
+++ src/test/kotlin/io/gvespucci/kotestpoc/KotestPocApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.gvespucci.kotestpoc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotestPocApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
